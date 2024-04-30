cec1d58b0980ee79af3b92381093227669cbad79
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-04-30 14:00:46 -0400

    fix section grab

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -63,7 +63,7 @@ test('Find Courses', async ({ page }) => {
   console.log(courseTitles);
 });
 
-test("Find Sections", async ({ page }) => {
+test("Find Sections", async ({ page, request }) => {
   await page.goto('https://prodss.suffolk.edu/Student/Courses/Search');
 
   const token = await page.$eval('input[name="__RequestVerificationToken"]', (el) => el.getAttribute('value'));
@@ -78,22 +78,73 @@ test("Find Sections", async ({ page }) => {
    * at TLSWrap.ssl.onhandshakedone -> code: 'UNABLE_TO_VERIFY_LEAF_SIGNATURE'
    */
 
+  /**
+   * Unable to verify leaf signature --> "unable to verify the first certificate"
+   *
+   * > openssl s_client -connect prodss.suffolk.edu:443 -servername prodss.suffolk.edu | tee logcertfile
+   *   - Certificate chain:
+   *      0 s:C = US, ST = Massachusetts, O = Suffolk University, CN = prodss.suffolk.edu
+   *      i:C = US, O = Internet2, CN = InCommon RSA Server CA 2
+   *
+   *   - with:
+   *      depth=0 C = US, ST = Massachusetts, O = Suffolk University, CN = prodss.suffolk.edu
+   *        verify error:num=20:unable to get local issuer certificate
+   *        verify return:1
+   *      depth=0 C = US, ST = Massachusetts, O = Suffolk University, CN = prodss.suffolk.edu
+   *        verify error:num=21:unable to verify the first certificate          <-- here!
+   *        verify return:1
+   *      depth=0 C = US, ST = Massachusetts, O = Suffolk University, CN = prodss.suffolk.edu
+   *        verify return:1
+   *
+   * NOTE: https://www.sslshopper.com/ssl-checker.html#hostname=prodss.suffolk.edu; at first glance, the certificate chain looks good?
+   *   1. prodss.suffolk.edu
+   *   2. InCommon RSA Server CA 2
+   *   3. USERTrust RSA Certification Authority
+   *
+   * To get the certs:
+   *
+   * > openssl s_client -connect prodss.suffolk.edu:443 -servername prodss.suffolk.edu | tee logcertfile
+   *   - save the cert chain to a file
+   *
+   * > openssl x509 -in logcertfile -noout -text | grep -i "issuer"
+   *   - get the issuer of the intermediate cert (seems like http://crt.sectigo.com/InCommonRSAServerCA2.crt)
+   *
+   * > curl --output intermediate.crt <url>
+   *   - download the intermediate cert
+   *
+   * > openssl x509 -inform DER -in intermediate.crt -out intermediate.pem -text
+   *   - convert to .pem (seems like .crt is really just .pem in another format (maybe DER, which is binary?)
+   *   - NOTE: do the same with root cert as well
+   *
+   * > openssl verify -CAfile intermediate.pem -untrusted root.pem prodss.suffolk.edu.crt
+   *   - should ideally verify the cert chain
+   *
+   * NOTE: last few steps are untested because it seems like Suffolk IT fixed the cert issue literally in the middle
+   *       of me trying to figure out what the fuck was happening. LOL
+   */
+
   try {
-    let res = await fetch("https://prodss.suffolk.edu/Student/Courses/SectionsAsync", {
+    let res = await request.post("https://prodss.suffolk.edu/Student/Courses/SectionsAsync", {
       "headers": {
         "__isguestuser": "true",
-        "__requestverificationtoken": token,
+        "__requestverificationtoken": "5CV94YdyV2iiR_TgfblJzIGlIAZ6tvKWBOztqSI83czq7aYbb7ODKzaMqmyZv_O2HIX1WIRypv97i_Ie23SuvprWkTz4FKA2rV0BzUZT4nE1",
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "en-US,en;q=0.9",
         "content-type": "application/json, charset=UTF-8",
-        "x-requested-with": "XMLHttpRequest"
+        "priority": "u=1, i",
+        "sec-ch-ua": "\"Not-A.Brand\";v=\"99\", \"Chromium\";v=\"124\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "cookie": "_ga=GA1.2.1654894405.1705425235; __RequestVerificationToken_L1N0dWRlbnQ1=relYR47ZDTWi9BV0t3sa2NiwZ1qPNw-DxBNwCgMvYVxy1QpVeO_Iw3zelEDXwsdQG4iMhOiS1dtzQ5u9lstM8JQIYS9-SBXKhfPWGA8DYbo1; _sp=%257B%2522id%2522%253A%2522232664a9-db26-483b-8270-720cb6d391a9%2522%257D",
+        "Referer": "https://prodss.suffolk.edu/Student/Courses/Search",
+        "Referrer-Policy": "strict-origin-when-cross-origin"
       },
-      "referrer": "https://prodss.suffolk.edu/Student/Courses/Search",
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": "{\"courseId\":\"7245\",\"sectionIds\":[\"160962\"]}",
-      "method": "POST",
-      "mode": "cors",
+      "data": "{\"courseId\":\"7245\",\"sectionIds\":[\"160962\"]}",
     });
-
-    console.log(res);
 
     let data = await res.json();
 
