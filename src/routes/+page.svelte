e4109a650940217551eb30774073a74768b0f326
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-05-01 22:02:56 -0400

    render page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<input
+  type="text"
+  placeholder="Search..."
+  on:input={(e) => {
+    const search = e.currentTarget.value.toLowerCase();
+    /**
+     * @type {NodeListOf<HTMLTableRowElement>}
+     */
+    const courses = document.querySelectorAll(".course");
+
+    courses.forEach((course) => {
+      const title =
+        course.querySelector("td:first-child")?.textContent?.toLowerCase() ??
+        "";
+      const section =
+        course.querySelector(".section")?.textContent?.toLowerCase() ?? "";
+
+      if (title.includes(search) || section.includes(search)) {
+        // @ts-ignore
+        course.style.display = "";
+      } else {
+        // @ts-ignore
+        course.style.display = "none";
+      }
+    });
+  }}
+/>
+
+<table>
+  <thead>
+    <tr>
+      <th style="width: 60ch;">Title</th>
+      <th style="width: 60ch;">Section</th>
+      <th style="width: 60ch;">Meeting Time</th>
+    </tr>
+  </thead>
+  {#each data.courses as course}
+    {@const meetings = new Set(course.meetings.map((m) => m.section))}
+    <tr style="border-top: 1px solid black;" class="course">
+      <td>{course.title}</td>
+      <td colspan="2">
+        {#each meetings as meeting}
+          {@const times = course.meetings
+            .filter((m) => m.section === meeting)
+            .map((m) => [m.startTime, m.endTime])}
+
+          {@const locs = course.meetings
+            .filter((m) => m.section === meeting)
+            .map((m) => m.location)}
+
+          {@const days = course.meetings
+            .filter((m) => m.section === meeting)
+            .map((m) => m.days)}
+
+          <table style="width: 100%;">
+            <tr>
+              <td style="width: 60ch;" class="section">
+                {meeting}
+              </td>
+              <td style="width: 60ch;">
+                {#each times as [startTime, endTime], i}
+                  <p>{days[i]}: {startTime} - {endTime} @ {locs[i]}</p>
+                {/each}
+              </td>
+            </tr>
+          </table>
+        {/each}
+      </td>
+    </tr>
+  {/each}
+</table>
