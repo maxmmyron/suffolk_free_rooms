37ae68d1d4a240c1ae470d7ec0a35a0773ab74d7
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-05-14 19:45:05 -0400

    add 3D room view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,35 @@
     };
   };
 
+  const floorMap = new Map([
+    // SAWYER: A, B, 1-11
+    [
+      "Sawyer",
+      [
+        "A",
+        "B",
+        ...Array.from({ length: 11 }).map((_, i) => (i + 1).toString()),
+      ],
+    ],
+    // STAHL: 1-9
+    [
+      "Rosalie K. Stahl Bldg",
+      [...Array.from({ length: 9 }).map((_, i) => (i + 1).toString())],
+    ],
+    // SAMIA: B, 1-8
+    [
+      "Samia Academic Center",
+      ["B", ...Array.from({ length: 8 }).map((_, i) => (i + 1).toString())],
+    ],
+    // SARGENT: 1-5
+    [
+      "Sargent Hall",
+      [...Array.from({ length: 5 }).map((_, i) => (i + 1).toString())],
+    ],
+    // BEACON: 1
+    ["1 Beacon", ["1"]],
+  ]);
+
   let date = new Date();
   $: time = date.getTime();
 
@@ -141,81 +170,189 @@
       getRelTime(section.endTime),
     ]);
   };
+
+  /**
+   * Gets the floor a room is on based on the building
+   * @param building
+   * @param rooms
+   */
+  const getAvailableFloorRooms = (
+    building: string,
+    rooms: [string, string][],
+    floor: string
+  ): [string, string][] => {
+    switch (building) {
+      case "Sawyer":
+        // EITHER XYY or XXYY, so trim away YY to get X/XX (floor)
+        return rooms.filter(([_, r]) => r.substring(0, r.length - 2) === floor);
+      case "Rosalie K. Stahl Bldg":
+        return rooms.filter(([_, r]) => r[0] === floor);
+      case "Samia Academic Center":
+        return rooms.filter(([_, r]) => r[0] === floor);
+      case "Sargent Hall":
+        return rooms.filter(([_, r]) => r[0] === floor);
+      default:
+        return rooms;
+    }
+  };
 </script>
 
-<div class="flex gap-2">
+<label>
   <input
     type="datetime-local"
     on:input={(e) => {
       date = new Date(e.currentTarget.value);
     }}
   />
-</div>
+  <output>{date.toString()}</output>
+</label>
 
-{#each Object.entries(data) as [building, rooms]}
-  {#each Object.entries(rooms) as [room, _]}
-    {#if availableRooms.findIndex((s) => s[0] === building && s[1] === room) !== -1}
-      {@const filledTimes = calcOccupiedTimes(building, room, time)}
-      {@const [startFree, endFree] = calcEmptyRoomTimes(building, room, time)}
-      <article class="room">
-        <h2>{building} {room}</h2>
-        <div>
-          {#each { length: 24 } as _, i}
-            <div class="tick" data-time="{i}:00"></div>
-          {/each}
-          {#each filledTimes as [startFilled, endFilled]}
-            {@const start =
-              (startFilled - getRelTime("00:00:00")) / 3600000 / 24}
-            {@const end = (endFilled - getRelTime("00:00:00")) / 3600000 / 24}
-            <div
-              style="left: {start * 100}%; right: {(1 - end) * 100}%;"
-              class="course-time"
-            ></div>
-          {/each}
-        </div>
-      </article>
+<div class="buildings">
+  {#each Object.entries(data) as [building, rooms]}
+    {@const availableBuildingRooms = availableRooms.filter(
+      (p) => p[0] === building
+    )}
+    {@const floors = floorMap.get(building)}
+    {#if floors}
+      <div class="building">
+        <h1>{building}</h1>
+        {#each floors.toReversed() as floor, i}
+          {@const availableFloorRooms = getAvailableFloorRooms(
+            building,
+            availableBuildingRooms,
+            floor
+          )}
+          <div
+            class="floor"
+            style="z-index: {floors.length * 2 - i * 2};"
+            data-has-rooms={availableFloorRooms.length !== 0}
+          >
+            <div class="face horz front">{floor}</div>
+            <div class="face horz back"></div>
+            <div class="face horz left"></div>
+            <div class="face horz right"></div>
+            <div class="face bottom"></div>
+          </div>
+          {#if availableFloorRooms.length}
+            <ul style="z-index: {floors.length * 2 - i};">
+              {#each availableFloorRooms as [building, room]}
+                {@const [startFree, endFree] = calcEmptyRoomTimes(
+                  building,
+                  room,
+                  time
+                )}
+                {@const startFreeTime = new Date(
+                  startFree
+                ).toLocaleTimeString()}
+                {@const endFreeTime = new Date(endFree).toLocaleTimeString()}
+                <li>
+                  {building}
+                  {room} <br />
+                  {startFreeTime} - {endFreeTime}
+                </li>
+              {/each}
+            </ul>
+          {/if}
+        {/each}
+      </div>
     {/if}
   {/each}
-{/each}
+</div>
 
 <style>
-  article.room {
+  .buildings {
     display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-    margin: 1rem 0;
-    padding-bottom: 0.5rem;
-    & h2 {
-      font-size: 1.25rem;
-      margin: 0;
+    flex-wrap: wrap;
+    align-items: flex-start;
+  }
+
+  .building {
+    display: grid;
+    grid-template-columns: 200px 300px;
+    justify-items: center;
+    align-items: flex-end;
+    flex-shrink: 0;
+    padding-bottom: 2rem;
+    border-bottom: 1px solid black;
+    margin-bottom: 2rem;
+    perspective: none;
+
+    & > h1 {
+      grid-area: 1/1/1/3;
     }
 
-    & > div {
-      display: grid;
-      grid-template-columns: repeat(24, 1fr);
-      height: 4rem;
+    & > .floor {
+      grid-column: 1;
       position: relative;
+      width: 100px;
+      height: 25px;
+      margin-bottom: -2px;
+      transform-style: preserve-3d;
+      transform: rotateY(46deg) rotateX(-12deg) rotateZ(-12deg);
 
-      background-color: rgba(240, 240, 240, 1);
-      border-radius: 12px;
-
-      & .tick {
-        position: relative;
-        border-left: 1px solid rgba(0, 0, 0, 0.25);
-        &::after {
-          position: absolute;
-          content: attr(data-time);
-          font-size: 0.7rem;
-          color: rgba(60, 60, 60, 1);
-          bottom: 0px;
-        }
+      &[data-had-rooms="true"] {
+        border-bottom: 1px dashed rgb(128, 128, 128);
       }
 
-      & .course-time {
+      & + ul {
+        position: relative;
+      }
+
+      /* target ul sibling */
+      &[data-has-rooms="true"] + ul::after {
+        --width: 120px;
+        --height: 16px;
+        content: "";
+        width: var(--width);
+        height: var(--height);
         position: absolute;
-        height: 50%;
-        background-color: blue;
+        bottom: 12px;
+        left: calc(20px - var(--width));
+        pointer-events: none;
+        border-radius: 8px 0 0 0;
+        border: 2px dashed rgb(40, 40, 40);
+        border-width: 2px 0 0 2px;
+      }
+
+      & > .face {
+        display: block;
+        position: absolute;
+        backface-visibility: visible;
+        outline: 1px solid transparent;
+      }
+
+      & > .horz {
+        width: 100px;
+        height: 25px;
+      }
+
+      & > .front {
+        background-color: rgb(128, 128, 128);
+        transform: translateZ(50px);
+        border-bottom: 1.5px solid rgba(0, 0, 0, 0.2);
+      }
+
+      & > .back {
+        background-color: rgb(100, 100, 100);
+        transform: rotateY(180deg) translateZ(50px);
+      }
+
+      & > .left {
+        background-color: rgb(200, 200, 200);
+        transform: rotateY(-90deg) translateZ(50px);
+        border-bottom: 1.5px solid rgba(0, 0, 0, 0.2);
+      }
+
+      & > .right {
+        background-color: rgb(80, 80, 80);
+        transform: rotateY(90deg) translateZ(50px);
+      }
+
+      & > .bottom {
+        width: 98px;
+        height: 98px;
+        background-color: rgb(40, 40, 40);
+        transform: rotateX(-90deg) translateZ(-30px) translateX(1px);
       }
     }
   }
