6f58608d7088d0085c8844a24c0708b61ed231db
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-05-16 20:32:48 -0400

    zoom to building

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -6,8 +6,12 @@
   import Building from "./Building.svelte";
   import { SheetObject } from "@threlte/theatre";
   import CameraAnimator from "./CameraAnimator.svelte";
-  import type { OrthographicCamera, PerspectiveCamera } from "three";
-  import { DEG2RAD } from "three/src/math/MathUtils.js";
+  import {
+    Sphere,
+    Vector3,
+    type OrthographicCamera,
+    type PerspectiveCamera,
+  } from "three";
 
   export let availableRooms: [string, string][];
 
@@ -24,40 +28,18 @@
   transitions();
 
   $: ((building: App.Building) => {
-    if (!$cameraAnimator) return;
-
-    switch (building) {
-      // TODO: finish me
-      case "Sawyer":
-        $cameraAnimator.rotate(-90 * DEG2RAD, 0, true);
-        break;
-      case "Rosalie K. Stahl Bldg":
-        $cameraAnimator.rotate(90 * DEG2RAD, 0, true);
-        break;
-      case "Samia Academic Center":
-        $cameraAnimator.rotate(-180 * DEG2RAD, 0, true);
-        break;
-      case "Sargent Hall":
-        $cameraAnimator.rotate(180 * DEG2RAD, 0, true);
-        break;
-      case "1 Beacon":
-        $cameraAnimator.rotate(270 * DEG2RAD, 0, true);
-        break;
+    if (!$cameraAnimator) {
+      console.log("Camera animator not found");
+      return;
     }
+
+    let sphere = new Sphere(
+      new Vector3(0, data.floors.length / 2, 0),
+      Math.max(data.floors.length, 6)
+    );
+
+    $cameraAnimator.fitToSphere(sphere, true);
   })($currentBuilding);
-
-  /**
-   * TODO: add smooth transition between prev and next cam pos
-   * position={[
-            10 + data.floors.length * 0.5,
-            camHeight,
-            -10 - data.floors.length * 0.5,
-          ]}
-
-          on:create={({ ref }) => {
-            ref.lookAt(0, data.floors.length / 2, 0);
-          }}
-  */
 </script>
 
 <Sky elevation={0.5} />
